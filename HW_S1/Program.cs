eb039f290dd3ce316ced8dbd8620e34f0e529267
sanya <shapovalo.susha@mil.ru>
2023-06-20 15:17:13 +0300

    HW1 gotovo

--- HW_S1/Program.cs
+++ HW_S1/Program.cs
@@ -6,11 +6,11 @@
             // int b = int.Parse(Console.ReadLine()!);
             // if (a>b) 
             // {
-            //   Console.Write($"(caislo {a} bolshe {b})");
+            //   Console.Write($"caislo {a} bolshe {b}");
             // }
             // else if (b>a)
             //      {
-            //         Console.Write($"(caislo {b} bolshe {a})");
+            //         Console.Write($"caislo {b} bolshe {a}");
             //      }
             //      else {
             //         Console.WriteLine("chisla ravni");
@@ -19,24 +19,46 @@
 
 // Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-            Console.Write("Vvedite pervoe chislo:");
-            int a = int.Parse(Console.ReadLine()!);
-            Console.Write("Vvedite vtoroe chislo:");
-            int b = int.Parse(Console.ReadLine()!);
-            Console.Write("Vvedite trete chislo:");
-            int c = int.Parse(Console.ReadLine()!);
-            int max = a;
-            if (b>max) {
-                max = b;
-            }
-            if (c>max) {
-                max = c;
-            }
-            Console.WriteLine($"(caislo {max} maximalnoe)");
+            // Console.Write("Vvedite pervoe chislo:");
+            // int a = int.Parse(Console.ReadLine()!);
+            // Console.Write("Vvedite vtoroe chislo:");
+            // int b = int.Parse(Console.ReadLine()!);
+            // Console.Write("Vvedite trete chislo:");
+            // int c = int.Parse(Console.ReadLine()!);
+            // int max = a;
+            // if (b>max) {
+            //     max = b;
+            // }
+            // if (c>max) {
+            //     max = c;
+            // }
+            // Console.WriteLine($"caislo {max} maximalnoe");
 // Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
-
+            // Console.Write("Vvedite chislo:");
+            // int a = int.Parse(Console.ReadLine()!);
+            // if (a%2 == 0) {
+            //     Console.WriteLine($"caislo {a} chetnoe");
+            // }
+            // else {
+            //     Console.WriteLine($"caislo {a} nechetnoe");
+            // }
 // Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
-
-
+                
+            // Console.Write("Vvedite chislo:");
+            // int N = int.Parse(Console.ReadLine()!);
+            // int index = 2;
+            // if (N>0){
+            //     while (index <= N) {
+            //         Console.Write($"{index} ");
+            //         index += 2;
+            //     }
+            // }    
+            // if (N<0){
+            //     index = -2;
+            //     while (index >= N) {
+            //         Console.Write($"{index} ");
+            //         index -= 2;
+            //     }               
+            // }
