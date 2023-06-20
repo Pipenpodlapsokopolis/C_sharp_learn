ca1e1a08a8abfc6088ce1f17066e0930da7c3b8f
sanya <shapovalo.susha@mil.ru>
2023-06-20 15:01:29 +0300

    shfewjwf

--- HW_S1/Program.cs
+++ HW_S1/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+            // Console.Write("Vvedite pervoe chislo:");
+            // int a = int.Parse(Console.ReadLine()!);
+            // Console.Write("Vvedite vtoroe chislo:");
+            // int b = int.Parse(Console.ReadLine()!);
+            // if (a>b) 
+            // {
+            //   Console.Write($"(caislo {a} bolshe {b})");
+            // }
+            // else if (b>a)
+            //      {
+            //         Console.Write($"(caislo {b} bolshe {a})");
+            //      }
+            //      else {
+            //         Console.WriteLine("chisla ravni");
+            //      }
+
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+            Console.Write("Vvedite pervoe chislo:");
+            int a = int.Parse(Console.ReadLine()!);
+            Console.Write("Vvedite vtoroe chislo:");
+            int b = int.Parse(Console.ReadLine()!);
+            Console.Write("Vvedite trete chislo:");
+            int c = int.Parse(Console.ReadLine()!);
+            int max = a;
+            if (b>max) {
+                max = b;
+            }
+            if (c>max) {
+                max = c;
+            }
+            Console.WriteLine($"(caislo {max} maximalnoe)");
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+
+
