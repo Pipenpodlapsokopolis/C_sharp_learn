d962e25bbfe4f97503c3d40726a70ac456b29414
sanya <shapovalo.susha@mil.ru>
2023-06-23 16:28:48 +0300

    HW3 gotovo

---
+++ HW_S3/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+                // Console.Write("Bведите число: ");
+                // int a = int.Parse(Console.ReadLine()!);
+                // if (a<10000 || a>99999){
+                // Console.WriteLine("Bведите корректное число");
+                // }
+                // else {
+                // int y = ((a%100)/10);
+                // int x = (a%10)*10+y;
+                // int z = a/1000;
+                // if (x == z){
+                // Console.WriteLine($"число {a} - палиндром");}
+                // else {
+                //     Console.WriteLine($"число {a} - не палиндром");
+                // }
+                // }
+// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+            // Console.WriteLine("Vvedite kordinatu X tochki A ");
+            // int ax = int.Parse(Console.ReadLine()!);
+            // Console.WriteLine("Vvedite kordinatu Y tochki A ");
+            // int ay = int.Parse(Console.ReadLine()!);
+            // Console.WriteLine("Vvedite kordinatu Z tochki A ");
+            // int az = int.Parse(Console.ReadLine()!);
+            // Console.WriteLine("Vvedite kordinatu X tochki B ");
+            // int bx = int.Parse(Console.ReadLine()!);
+            // Console.WriteLine("Vvedite kordinatu Y tochki B ");
+            // int by = int.Parse(Console.ReadLine()!);
+            // Console.WriteLine("Vvedite kordinatu Z tochki B ");
+            // int bz = int.Parse(Console.ReadLine()!);
+
+            // double ab = Math.Sqrt(Math.Pow((ax-bx), 2)+Math.Pow((ay-by), 2)+Math.Pow((az-bz), 2));
+
+            // Console.WriteLine($"Rasstoyanie ot A do B ravno {ab:f2}");
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+        // Console.Write("Bведите число: ");
+        // int a = int.Parse(Console.ReadLine()!);
+        // int num = 0;
+        // if (a<0){
+        // Console.WriteLine("Bведите корректное число");
+        // }
+        // else {
+
+        // for (int i = 1; i <= a; i++)
+        // {
+        // num = i*i*i;
+        // Console.Write($"{num}  ");
+        // }
+        // }
