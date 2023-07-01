f3475e468a270f6ba31cb50d0ec93044f4ffac48
sanya <shapovalo.susha@mil.ru>
2023-07-01 08:30:30 +0300

    HW tochno gotovo

--- HW_S5/Program.cs
+++ HW_S5/Program.cs
@@ -63,30 +63,30 @@
 // [22, 42, 15, 77, 65] => 77 - 15 = 62**
 
 
-                // int[] GetArray(int size){
-                // int[] result = new int[size];
-                // for(int i = 0; i < size; i++){
-                // result[i] = new Random().Next(99, 1000);
-                // }
-                // return result;
-                // }
+                int[] GetArray(int size){
+                int[] result = new int[size];
+                for(int i = 0; i < size; i++){
+                result[i] = new Random().Next(9, 100);
+                }
+                return result;
+                }
                 
-                // Console.Write("Vvedite razmer massiva: ");
-                // int size = int.Parse(Console.ReadLine()!);
+                Console.Write("Vvedite razmer massiva: ");
+                int size = int.Parse(Console.ReadLine()!);
                 
-                // int[] array = GetArray(size);
-                // Console.WriteLine($"[{String.Join(", ", array)}]"); 
-                //                     int max = 0;
-                //     int min = array[1];
-                //     int chenge = 0;
-                //     for (int i = 0; i < size; i++)
-                //     {
-                //         if (array[i]>max){
-                //             max = array[i];
-                //         }
-                //         if (array[i]<min){
-                //             min = array[i];
-                //         }
-                //     }
-                // chenge = max -min;
-                // Console.WriteLine($"Pазницa между максимальным и минимальным элементов массива: {chenge}");
+                int[] array = GetArray(size);
+                Console.WriteLine($"[{String.Join(", ", array)}]"); 
+                                    int max = 0;
+                    int min = array[1];
+                    int chenge = 0;
+                    for (int i = 0; i < size; i++)
+                    {
+                        if (array[i]>max){
+                            max = array[i];
+                        }
+                        if (array[i]<min){
+                            min = array[i];
+                        }
+                    }
+                chenge = max -min;
+                Console.WriteLine($"Pазницa между максимальным и минимальным элементов массива: {chenge}");
